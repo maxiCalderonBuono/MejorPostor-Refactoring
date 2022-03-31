43dfb153635bc373dd937f07bfca497b45ef25c0
LisandroPer <87787218+LisandroPer@users.noreply.github.com>
2022-03-31 16:53:56 -0300

    Primer prueba de carousel de productos. Todavía falta mejorar el responsive y añadir varias cosas

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import { NavBar } from "./components/organisms/NavBar";
 import ProductListContainer from "./components/moleculs/Cards/ProductListContainer";
 
+import "swiper/css/bundle";
+
+
 function App() {
   return (
     <div className="flex justify-center w-full h-full p-8 md:h-full bg-background_main">
       <NavBar />
       <ProductListContainer />
+      
+
     </div>
   );
 }
--- src/components/moleculs/Cards/ProductList.js
+++ src/components/moleculs/Cards/ProductList.js
@@ -1,21 +1,52 @@
 import React from "react";
 import CardProduct from "./CardProduct";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import ProductListContainer from "./ProductListContainer";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+
+import "../../../screens/pujas.css";
+
+// import required modules
+import { Grid, Pagination } from "swiper";
 
 // Función para mapear el componente Card con todas las popiedades que se le pasen. EN PROCESO.
 const ProductList = ({ products }) => {
     return (
         <>
-            {
-                products.map(product => (
-                    <CardProduct
-                        key={product.id}
-                        id={product.id}
-                        title={product.title}
-                        price={product.price}
-                        image={product.thumbnail}
-                    />
-                ))
-            }
+            <Swiper
+            slidesPerView={3}
+            grid={{
+            rows: 2,
+            }}
+            spaceBetween={30}
+            pagination={{
+            clickable: true,
+            }}
+            modules={[Grid, Pagination]}
+            className="mySwiper"
+        >
+                { products.length > 0 ? (
+                    products.map(product => (
+                        <SwiperSlide>
+                        <CardProduct
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            price={product.price}
+                            image={product.thumbnail}
+                        />
+                        </SwiperSlide>
+                    ))
+                )
+                : (<p>Cargando Productos...</p>)
+                }
+
+             </Swiper>
         </>
     );
 };
