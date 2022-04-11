6d36c3806e1aa2174e7e720620e7e50243e90505
LisandroPer <87787218+LisandroPer@users.noreply.github.com>
2022-04-11 16:33:27 -0300

    ProductDetail fase beta

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { AppRouter } from "./routers/AppRouter";
 import "swiper/css/bundle";
 import { startIsAuth } from "./actions/auth";
-
+import ProductDetail from "./components/moleculs/ProductDetail";
 const App = () => {
   const dispatch = useDispatch();
 
@@ -13,9 +13,7 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full md:h-full bg-background_main">
-      <AppRouter />
-    </div>
+    <ProductDetail />
   );
 };
 
