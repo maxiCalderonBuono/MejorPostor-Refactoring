be884bae466e9342180e7c1cc9515351f236324b
Maxi <calderonmaxi@outlook.com>
2022-04-17 10:18:05 -0300

    fix: edit profile and new bid aligment

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   
   return (
     <Provider store={store}>
-      <div className="flex flex-col justify-center w-full h-full md:h-full bg-background_main">
+      <div className="flex flex-col items-center justify-center w-full h-full md:h-full bg-background_main">
         <AppRouter />
       </div>
     </Provider>
