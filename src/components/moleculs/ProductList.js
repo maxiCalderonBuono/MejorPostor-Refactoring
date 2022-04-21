dc84e21eaee7f8deacf44936783b187d5522e03b
Alejandro Pedrosa <alejandro.pedrosa@hotmail.com>
2022-04-21 18:30:56 -0300

    Fix fechas y new post redirect

--- src/components/moleculs/ProductList.js
+++ src/components/moleculs/ProductList.js
@@ -3,7 +3,6 @@ import CardProduct from "./CardProduct";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/grid";
@@ -15,56 +14,48 @@ import "../../screens/pujas.css";
 import { Grid, Pagination } from "swiper";
 
 // Función para mapear el componente Card con todas las popiedades que se le pasen. EN PROCESO.
-const ProductList = ( {products} ) => {
-
-
-
-    return (
-        <>
-            <Swiper
-
-            breakpoints={{
-                640: {
-                width: 640,
-                slidesPerView: 1,
-                },
-                768: {
-                width: 768,
-                slidesPerView: 2,
-                },
-               
-            }}
-
-            spaceBetween={30}
-            pagination={{
-            clickable: true,
-            }}
-            modules={[Grid, Pagination]}
-            className="mySwiper"
-        >
-                { products.length > 0 ? (
-                    products.map((product, index) => (
-                    
-                        <SwiperSlide  key={index}>
-                            <CardProduct
-                                name={product.name}
-                                image={product.image}
-                                description={product.description}
-                                initialPrice={product.initialPrice}
-                                user={product.user}
-                                createdAt={product.createdAt}
-                                _id={product._id}
-                                duration={product.duration}
-                                highestBid={product.highestBid}
-                            />
-                        </SwiperSlide>
-                    ))
-                )
-                : (<p>Cargando Productos...</p>)
-                }
-
-             </Swiper>
-        </>
-    );
+const ProductList = ({ products }) => {
+  return (
+    <>
+      <Swiper
+        breakpoints={{
+          640: {
+            width: 640,
+            slidesPerView: 1,
+          },
+          768: {
+            width: 768,
+            slidesPerView: 2,
+          },
+        }}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Grid, Pagination]}
+        className="mySwiper"
+      >
+        {products.length > 0 ? (
+          products.map((product, index) => (
+            <SwiperSlide key={index}>
+              <CardProduct
+                name={product.name}
+                image={product.image}
+                description={product.description}
+                initialPrice={product.initialPrice}
+                user={product.user}
+                createdAt={product.createdAt}
+                _id={product._id}
+                duration={product.duration}
+                highestBid={product.highestBid}
+              />
+            </SwiperSlide>
+          ))
+        ) : (
+          <p>Cargando Productos...</p>
+        )}
+      </Swiper>
+    </>
+  );
 };
 export default ProductList;
